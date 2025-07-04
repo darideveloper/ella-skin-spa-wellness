df58eb9976d93adedef0583b6664cea8a1281454
daridev <darideveloper@gmail.com>
2025-07-03 18:50:48 -0600

    feat (Services): get texts from translations

---
+++ src/data/servcies.ts
@@ -0,0 +1,16 @@
+import { HiHeart, HiStar, HiSparkles, HiUserGroup } from 'react-icons/hi'
+
+export const servicesData = {
+  facial: {
+    icon: HiHeart,
+  },
+  therapy: {
+    icon: HiStar,
+  },
+  massage: {
+    icon: HiSparkles,
+  },
+  advanced: {
+    icon: HiUserGroup,
+  },
+}
