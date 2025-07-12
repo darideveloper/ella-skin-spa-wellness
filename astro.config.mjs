55e24f4536d0514c4e3a575b5c1eea43901bcd99
daridev <darideveloper@gmail.com>
2025-07-12 15:26:49 -0600

    feat (settings): setup robots and sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,34 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 
-import react from '@astrojs/react';
+import react from '@astrojs/react'
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from '@tailwindcss/vite'
 
-import showTailwindcssBreakpoint from 'astro-show-tailwindcss-breakpoint';
+import showTailwindcssBreakpoint from 'astro-show-tailwindcss-breakpoint'
+
+import sitemap from '@astrojs/sitemap'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), showTailwindcssBreakpoint()],
-
+  site: 'https://ella-skin-spa-wellness.com',
+  integrations: [react(), showTailwindcssBreakpoint(), sitemap({
+    i18n: {
+      defaultLocale: 'es',
+      locales: {
+        en: 'en',
+        es: 'es',
+      },
+    },
+    changefreq: 'weekly',
+    priority: 1,
+    lastmod: new Date()
+  })],
   vite: {
     plugins: [tailwindcss()]
+  },
+  i18n: {
+    locales: ["es", "en"],
+    defaultLocale: "es",
   }
-});
+})
