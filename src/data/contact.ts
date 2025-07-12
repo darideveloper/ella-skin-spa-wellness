e7d65958afb8dec7c34b6898ba61ad0550dca83b
daridev <darideveloper@gmail.com>
2025-07-12 15:52:21 -0600

    feat (SEO): setup json ld

--- src/data/contact.ts
+++ src/data/contact.ts
@@ -1,1 +1,43 @@
-export const phone = '5219999049631'
+import { FaFacebook, FaInstagram, FaTwitter, FaYoutube } from 'react-icons/fa'
+
+export const phoneUnformatted = '+57 317 828 28 28'
+export const phone = phoneUnformatted.replace(/\s/g, '').replace('+', '')
+
+export const email = 'info@ellaskinspa.com'
+
+export const addressElems = {
+  addressLocality: 'Merida',
+  addressRegion: 'Yucatan',
+  addressCountry: 'Mexico',
+}
+export let address = ''
+for (const elem in addressElems) {
+  address += `${elem}: ${addressElems[elem as keyof typeof addressElems]}\n`
+}
+
+export const socialNetworks = [
+  {
+    icon: FaFacebook,
+    name: 'Facebook',
+    link: '#',
+    bg: 'bg-blue-600',
+  },
+  {
+    icon: FaInstagram,
+    name: 'Instagram',
+    link: '#',
+    bg: 'bg-gradient-to-r from-purple-500 to-pink-500',
+  },
+  {
+    icon: FaTwitter,
+    name: 'Twitter',
+    link: '#',
+    bg: 'bg-blue-400',
+  },
+  {
+    icon: FaYoutube,
+    name: 'YouTube',
+    link: '#',
+    bg: 'bg-red-600',
+  },
+]
---
+++ src/data/setup.ts
@@ -0,0 +1,1 @@
+export const domain = 'https://ella-skin-spa-wellness.com'
